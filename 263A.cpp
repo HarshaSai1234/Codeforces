fec67fbe042a473b424ef5e5b0f29b3fc32e3fd0
HarshaSai03 <2400030037@kluniversity.in>
2026-06-19 16:00:56 +0530

    19-06-26

---
+++ 263A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5][5];
+    int a=0,b=0;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            int x;
+            cin>>x;
+            arr[i][j]=x;
+            if(x==1){
+                a=i;
+                b=j;
+            }
+        }
+    }
+    cout<<abs(a-2)+abs(b-2)<<endl;
+}
