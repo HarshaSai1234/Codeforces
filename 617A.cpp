c4755e2f0fbbe03c40fd68a6bf9e61052fea5943
HarshaSai03 <harshap3712@gmail.com>
2026-06-29 19:35:05 +0530

    29-06-2026

---
+++ 617A.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/problemset/problem/617/A
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<ceil(a/5.0);
+    return 0;
+}
