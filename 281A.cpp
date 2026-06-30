5c602f7aaddb5381d40c013852dc8caf6ffda0d7
HarshaSai03 <harshap3712@gmail.com>
2026-06-30 19:15:36 +0530

    30-06-2026

---
+++ 281A.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/problemset/problem/281/A
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    s[0]=toupper(s[0]);
+    cout<<s;
+
+    return 0;
+}
