f934e58b3944a9f2ba76eb8c806fafea68a52419
HarshaSai03 <harshap3712@gmail.com>
2026-07-14 19:27:44 +0530

    14-7-2026

---
+++ 61A.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/problemset/problem/61/A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] == b[i])
+            cout << 0;
+        else
+            cout << 1;
+    }
+
+    return 0;
+}
