86b85a5bc41c1c81aa1451b88aaf4ffe00275d42
HarshaSai03 <harshap3712@gmail.com>
2026-07-20 14:52:00 +0530

    20-7-2026

---
+++ 1742A.cpp
@@ -0,0 +1,21 @@
+//https://codeforces.com/problemset/problem/1742/A
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c || a+c==b || b+c==a){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
