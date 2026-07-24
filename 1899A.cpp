54e826305879206a9514bd4e27ba72c1ec43f34d
HarshaSai03 <harshap3712@gmail.com>
2026-07-24 21:00:48 +0530

    24-7-2026

---
+++ 1899A.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/problemset/problem/1899/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if((n-10)%3==0 || (n+10)%3==0){
+            cout<<"First"<<endl;
+        }
+        else{
+            cout<<"Second"<<endl;
+        }
+    }
+}
