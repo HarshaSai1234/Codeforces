387ac13eb83f8e063623e773819b869e80087a61
HarshaSai03 <harshap3712@gmail.com>
2026-08-01 07:17:16 +0530

    1-8-2026

---
+++ 1915A.cpp
@@ -0,0 +1,23 @@
+//https://codeforces.com/problemset/problem/1915/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a==b){
+            cout<<c<<endl;
+        }
+        else if(b==c){
+            cout<<a<<endl;
+        }
+        else{
+            cout<<b<<endl;
+        }
+    }
+}
