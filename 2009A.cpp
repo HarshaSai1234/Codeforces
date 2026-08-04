3b488fb66b0c8ced03060764e25481280a1facf1
HarshaSai03 <harshap3712@gmail.com>
2026-08-04 07:30:48 +0530

    4-8-2026

---
+++ 2009A.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/problemset/problem/2009/A
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << b - a << endl;
+    }
+
+    return 0;
+}
