df0633098245d6e4b1e8f0e3a6243885d9f5b762
HarshaSai03 <harshap3712@gmail.com>
2026-08-05 18:04:13 +0530

    5-8-2026

---
+++ 1873A.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/problemset/problem/1873/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s[0]=='a'||s[1]=='b'||s[2]=='c'){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+}
