0b57c63711c16c8950ce7c93d9c55066138f9a44
HarshaSai03 <harshap3712@gmail.com>
2026-08-05 17:56:30 +0530

    5-8-2026

---
+++ 1985A.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/problemset/problem/1985/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string a,b;
+        cin>>a>>b;
+        char t = a[0];
+        a[0]=b[0];
+        b[0]=t;
+        cout<<a<<" "<<b<<endl;
+    }
+}
