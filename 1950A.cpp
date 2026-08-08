1efae47111e0bae4b60b668864f308e9c0a8f886
HarshaSai03 <harshap3712@gmail.com>
2026-08-08 07:31:30 +0530

    8-8-2026

---
+++ 1950A.cpp
@@ -0,0 +1,23 @@
+//https://codeforces.com/problemset/problem/1950/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a<b && b<c){
+            cout<<"STAIR\n";
+        }
+        else if(a<b &&b>c){
+            cout<<"PEAK\n";
+        }
+        else{
+            cout<<"NONE\n";
+        }
+    }
+}
