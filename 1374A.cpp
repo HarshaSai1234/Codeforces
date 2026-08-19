e5f3b170c15c38eb43549fc3242e4f57bb9b17af
HarshaSai03 <harshap3712@gmail.com>
2026-08-19 19:14:05 +0530

    19-08-2026

---
+++ 1374A.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/problemset/problem/1374/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        long long x,y,n;
+        cin>>x>>y>>n;
+        int ans = n - (n - y) % x;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
