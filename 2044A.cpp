9827bb78f77fff5504074be4096250b3aec3618e
HarshaSai03 <harshap3712@gmail.com>
2026-08-22 08:22:15 +0530

    22-08-2026

---
+++ 2044A.cpp
@@ -0,0 +1,15 @@
+//https://codeforces.com/problemset/problem/2044/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n-1<<endl;
+    }
+}
