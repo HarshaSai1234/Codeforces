6b754351eadd217ccb411a2371fa8e94715952bb
HarshaSai03 <harshap3712@gmail.com>
2026-08-24 16:29:45 +0530

    24-08-2026

---
+++ 1807B.cpp
@@ -0,0 +1,28 @@
+//https://codeforces.com/contest/1807/problem/B
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        int ec=0;
+        int oc=0;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]%2==0) ec+=a[i];
+            else oc+=a[i];
+        }
+        if(ec>oc){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+}
