e3c218db69796b363b29d2470175382845262e49
HarshaSai03 <harshap3712@gmail.com>
2026-08-24 16:30:57 +0530

    24-08-2026

---
+++ 1834A.cpp
@@ -0,0 +1,39 @@
+//https://codeforces.com/contest/1834/problem/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        int pos = 0, neg = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+ 
+            if (x == 1)
+                pos++;
+            else
+                neg++;
+        }
+ 
+        int ans = 0;
+ 
+        while (neg > pos || neg % 2 != 0) {
+            neg--;
+            pos++;
+            ans++;
+        }
+ 
+        cout << ans << endl;
+    }
+ 
+    return 0;
+}
