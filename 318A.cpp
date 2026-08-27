84c4cc6030e64a2254f4b556b0f3c898a3db86a4
HarshaSai03 <harshap3712@gmail.com>
2026-08-27 18:53:54 +0530

    27-08-2026

---
+++ 318A.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/contest/318/problem/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    long long n, k;
+    cin >> n >> k;
+ 
+    long long oddCount = (n + 1) / 2;
+ 
+    if (k <= oddCount) {
+        cout << 2 * k - 1;
+    } else {
+        cout << 2 * (k - oddCount);
+    }
+ 
+    return 0;
+}
