f5dc92502e80be969bb7a8d263b5f85d5e38d617
HarshaSai03 <harshap3712@gmail.com>
2026-09-01 17:34:40 +0530

    1-09-2026

---
+++ 405A.cpp
@@ -0,0 +1,21 @@
+//https://codeforces.com/contest/405/problem/A
+
+
+//https://codeforces.com/problemset/problem/405/A
+ 
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    for(int i:v){
+        cout<<i<<" ";
+    }
+}
