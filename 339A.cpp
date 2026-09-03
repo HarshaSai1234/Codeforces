9df9e9486bf65d84d2d41c102a6506e77e15cecb
HarshaSai03 <harshap3712@gmail.com>
2026-09-03 20:45:48 +0530

    3-09-2026

---
+++ 339A.cpp
@@ -0,0 +1,21 @@
+//https://codeforces.com/problemset/problem/339/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    string nums;
+    for (char c : s) if (c != '+') nums += c;
+    
+    sort(nums.begin(), nums.end());
+    
+    for (int i = 0; i < nums.size(); i++) {
+        if (i) cout << "+";
+        cout << nums[i];
+    }
+    cout << endl;
+}
